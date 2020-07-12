a74f6e3751ff9cd7f762cab65853584d274b6640
Mark Whitaker <mark@mainwave.co.uk>
2020-07-12 11:03:35 +0100

    Reimplemented HtmlService

--- SaturdayQuizWeb.UnitTests/Services/HtmlServiceTests.cs
+++ SaturdayQuizWeb.UnitTests/Services/HtmlServiceTests.cs
@@ -1,154 +1,79 @@
 ﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using NSubstitute;
 using NUnit.Framework;
 using SaturdayQuizWeb.Model;
+using SaturdayQuizWeb.Model.Parsing;
 using SaturdayQuizWeb.Services;
+using SaturdayQuizWeb.Services.Parsing;
 
 namespace SaturdayQuizWeb.UnitTests.Services
 {
     [TestFixture]
     public class HtmlServiceTests
     {
-        private readonly IHtmlService _htmlService = new HtmlService();
-        private readonly string _html;
-        private IList<Question> _questions;
-
-        public HtmlServiceTests()
-        {
-            _html = File.ReadAllText(TestContext.CurrentContext.TestDirectory + "/TestData/2019_07_20_quiz.html");
-        }
+        private ISectionExtractor _mockSectionExtractor;
+        private IHtmlStripper _mockHtmlStripper;
+        private ISectionSplitter _mockSectionSplitter;
+        private IQuestionAssembler _mockQuestionAssember;
+        
+        private IHtmlService _htmlService;
 
         [SetUp]
         public void SetUp()
         {
-            _questions = _htmlService.FindQuestions(_html).ToList();
+            _mockSectionExtractor = Substitute.For<ISectionExtractor>();
+            _mockHtmlStripper = Substitute.For<IHtmlStripper>();
+            _mockSectionSplitter = Substitute.For<ISectionSplitter>();
+            _mockQuestionAssember = Substitute.For<IQuestionAssembler>();
+            
+            _htmlService = new HtmlService(
+                _mockSectionExtractor,
+                _mockHtmlStripper,
+                _mockSectionSplitter,
+                _mockQuestionAssember);
         }
-        
+
         [Test]
-        public void GivenQuizIsLoaded_WhenQuestionCount_ThenCountIs15()
+        public void GivenHtml_WhenFindQuestions_ThenExpectedQuestionsAreReturned()
         {
-            Assert.That(_questions.Count, Is.EqualTo(15));
-        }
+            // Given
+            const string html = "html";
+            const string expectedQuestionsHtml = "questionsHtml";
+            const string expectedAnswersHtml = "answersHtml";
+            const string expectedQuestionsHtmlStripped = "questionsHtmlStripped";
+            const string expectedAnswersHtmlStripped = "answersHtmlStripped";
+            var expectedSections = new Sections
+            {
+                QuestionsSectionHtml = expectedQuestionsHtml,
+                AnswersSectionHtml = expectedAnswersHtml
+            };
+            var expectedQuestionsList = new List<string> {"questions"};
+            var expectedAnswersList = new List<string> {"answers"};
+            var expectedQuestions = new List<Question>
+            {
+                new Question
+                {
+                    Number = 1,
+                    Type = QuestionType.Normal,
+                    QuestionHtml = "questionHtml",
+                    QuestionText = "questionText",
+                    AnswerHtml = "answerHtml",
+                    AnswerText = "answerText"
+                }
+            };
 
-        [TestCase(0, 1)]
-        [TestCase(1, 2)]
-        [TestCase(2, 3)]
-        [TestCase(3, 4)]
-        [TestCase(4, 5)]
-        [TestCase(5, 6)]
-        [TestCase(6, 7)]
-        [TestCase(7, 8)]
-        [TestCase(8, 9)]
-        [TestCase(9, 10)]
-        [TestCase(10, 11)]
-        [TestCase(11, 12)]
-        [TestCase(12, 13)]
-        [TestCase(13, 14)]
-        [TestCase(14, 15)]
-        public void GivenQuizIsLoaded_WhenQuestionNumber_ThenNumberIsCorrect(int questionIndex, int expectedQuestionNumber)
-        {
-            Assert.That(_questions[questionIndex].Number, Is.EqualTo(expectedQuestionNumber));
-        }
-
-        [TestCase(0, QuestionType.Normal)]
-        [TestCase(1, QuestionType.Normal)]
-        [TestCase(2, QuestionType.Normal)]
-        [TestCase(3, QuestionType.Normal)]
-        [TestCase(4, QuestionType.Normal)]
-        [TestCase(5, QuestionType.Normal)]
-        [TestCase(6, QuestionType.Normal)]
-        [TestCase(7, QuestionType.Normal)]
-        [TestCase(8, QuestionType.WhatLinks)]
-        [TestCase(9, QuestionType.WhatLinks)]
-        [TestCase(10, QuestionType.WhatLinks)]
-        [TestCase(11, QuestionType.WhatLinks)]
-        [TestCase(12, QuestionType.WhatLinks)]
-        [TestCase(13, QuestionType.WhatLinks)]
-        [TestCase(14, QuestionType.WhatLinks)]
-        public void GivenQuizIsLoaded_WhenQuestionType_ThenTypeIsCorrect(int questionIndex, QuestionType expectedQuestionType)
-        {
-            Assert.That(_questions[questionIndex].Type, Is.EqualTo(expectedQuestionType));
-        }
-
-        [TestCase(0, "Which Nazi leader died in Paddington in 1981?")]
-        [TestCase(1, "What are produced at La Masia & La Fábrica?")]
-        [TestCase(2, "In publishing, what does ISBN stand for?")]
-        [TestCase(3, "Adopted in 1625, what symbol is the Dannebrog?")]
-        [TestCase(4, "Gabriele Münter was a founder member of what expressionist group?")]
-        [TestCase(5, "What was nicknamed the Honourable John Company?")]
-        [TestCase(6, "Which country separates Guyana and French Guiana?")]
-        [TestCase(7, "In what novel is Constance unhappily married to Sir Clifford?")]
-        [TestCase(8, "Asgard and Midgard, in the form of a rainbow?")]
-        [TestCase(9, "Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?")]
-        [TestCase(10, "Statant; sejant; rampant; passant; dormant?")]
-        [TestCase(11, "Victoria Embankment; Cardiff City Hall; Colchester station?")]
-        [TestCase(12, "Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?")]
-        [TestCase(13, "Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?")]
-        [TestCase(14, "Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?")]
-        public void GivenQuizIsLoaded_WhenQuestionText_ThenTextIsCorrect(int questionIndex, string expectedQuestionText)
-        {
-            Assert.That(_questions[questionIndex].QuestionText, Is.EqualTo(expectedQuestionText));
-        }
-
-        [TestCase(0, "Which Nazi leader died in Paddington in 1981?")]
-        [TestCase(1, "What are produced at <i>La Masia</i> &amp; <i>La Fábrica</i>?")]
-        [TestCase(2, "In publishing, what does ISBN stand for?")]
-        [TestCase(3, "Adopted in 1625, what symbol is the Dannebrog?")]
-        [TestCase(4, "Gabriele Münter was a founder member of what expressionist group?")]
-        [TestCase(5, "What was nicknamed the Honourable John Company?")]
-        [TestCase(6, "Which country separates Guyana and French Guiana?")]
-        [TestCase(7, "In what novel is Constance unhappily married to Sir Clifford?")]
-        [TestCase(8, "Asgard and Midgard, in the form of a rainbow?")]
-        [TestCase(9, "Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?")]
-        [TestCase(10, "Statant; sejant; rampant; passant; dormant?")]
-        [TestCase(11, "Victoria Embankment; Cardiff City Hall; Colchester station?")]
-        [TestCase(12, "Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?")]
-        [TestCase(13, "Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?")]
-        [TestCase(14, "Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?")]
-        public void GivenQuizIsLoaded_WhenQuestionHtml_ThenHtmlIsCorrect(int questionIndex, string expectedQuestionHtml)
-        {
-            Assert.That(_questions[questionIndex].QuestionHtml, Is.EqualTo(expectedQuestionHtml));
-        }
-
-        [TestCase(0, "Albert Speer")]
-        [TestCase(1, "Footballers (academies of Barcelona and Real Madrid)")]
-        [TestCase(2, "International Standard Book Number")]
-        [TestCase(3, "Danish flag")]
-        [TestCase(4, "Der Blaue Reiter (Blue Rider)")]
-        [TestCase(5, "East India Company")]
-        [TestCase(6, "Suriname")]
-        [TestCase(7, "Lady Chatterley’s Lover")]
-        [TestCase(8, "Bifrost (bridge in Norse myth, linking gods’ realm and Earth)")]
-        [TestCase(9, "Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson")]
-        [TestCase(10, "Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down")]
-        [TestCase(11, "Statues of Boudicca")]
-        [TestCase(12, "Parts of Mount Everest")]
-        [TestCase(13, "Prime: canonical hour of prayer; prime meridian; prime numbers")]
-        [TestCase(14, "Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead")]
-        public void GivenQuizIsLoaded_WhenAnswerText_ThenTextIsCorrect(int questionIndex, string expectedAnswerText)
-        {
-            Assert.That(_questions[questionIndex].AnswerText, Is.EqualTo(expectedAnswerText));
-        }
-
-        [TestCase(0, "Albert Speer")]
-        [TestCase(1, "Footballers (academies of <i>Barcelona</i> and <i>Real Madrid</i>)")]
-        [TestCase(2, "International Standard Book Number")]
-        [TestCase(3, "Danish flag")]
-        [TestCase(4, "Der Blaue Reiter (Blue Rider)")]
-        [TestCase(5, "East India Company")]
-        [TestCase(6, "Suriname")]
-        [TestCase(7, "Lady Chatterley’s Lover")]
-        [TestCase(8, "Bifrost (bridge in Norse myth, linking gods’ realm and Earth)")]
-        [TestCase(9, "Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson")]
-        [TestCase(10, "Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down")]
-        [TestCase(11, "Statues of Boudicca")]
-        [TestCase(12, "Parts of Mount Everest")]
-        [TestCase(13, "Prime: canonical hour of prayer; prime meridian; prime numbers")]
-        [TestCase(14, "Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead")]
-        public void GivenQuizIsLoaded_WhenAnswerHtml_ThenHtmlIsCorrect(int questionIndex, string expectedAnswerHtml)
-        {
-            Assert.That(_questions[questionIndex].AnswerHtml, Is.EqualTo(expectedAnswerHtml));
+            _mockSectionExtractor.ExtractSections(html).Returns(expectedSections);
+            _mockHtmlStripper.StripHtml(expectedQuestionsHtml).Returns(expectedQuestionsHtmlStripped);
+            _mockHtmlStripper.StripHtml(expectedAnswersHtml).Returns(expectedAnswersHtmlStripped);
+            _mockSectionSplitter.SplitSection(expectedQuestionsHtmlStripped).Returns(expectedQuestionsList);
+            _mockSectionSplitter.SplitSection(expectedAnswersHtmlStripped).Returns(expectedAnswersList);
+            _mockQuestionAssember.AssembleQuestions(expectedQuestionsList, expectedAnswersList).Returns(expectedQuestions);
+            
+            // When
+            var actualQuestions = _htmlService.FindQuestions(html);
+            
+            // Then
+            Assert.That(actualQuestions, Is.EqualTo(expectedQuestions));
         }
     }
 }
--- SaturdayQuizWeb/Services/HtmlService.cs
+++ SaturdayQuizWeb/Services/HtmlService.cs
@@ -13,173 +13,45 @@ using RegexOptions = RegexToolbox.RegexOptions;
 
 namespace SaturdayQuizWeb.Services
 {
+    public interface IHtmlService
+    {
+        IEnumerable<Question> FindQuestions(string html);
+    }
+
     public class HtmlService : IHtmlService
     {
-        private const int MinQuestionCount = 15;
-        private static readonly IEnumerable<int> MinQuestionNumbers = Enumerable.Range(1, MinQuestionCount);
+        private readonly ISectionExtractor _sectionExtractor;
+        private readonly IHtmlStripper _htmlStripper;
+        private readonly ISectionSplitter _sectionSplitter;
+        private readonly IQuestionAssembler _questionAssembler;
 
-        private static readonly Regex WhatLinksRegex = new RegexBuilder()
-            .Text("what")
-            .PossibleHtmlWhitespace()
-            .Text("links")
-            .Text(":", ZeroOrOne)
-            .BuildRegex(RegexOptions.IgnoreCase);
+        public HtmlService(
+            ISectionExtractor sectionExtractor,
+            IHtmlStripper htmlStripper,
+            ISectionSplitter sectionSplitter,
+            IQuestionAssembler questionAssembler)
+        {
+            _sectionExtractor = sectionExtractor;
+            _htmlStripper = htmlStripper;
+            _sectionSplitter = sectionSplitter;
+            _questionAssembler = questionAssembler;
+        }
 
-        private static readonly Regex ATagRegex = BuildTagRegex("a");
-        private static readonly Regex BrTagRegex = BuildTagRegex("br");
-        private static readonly Regex PTagRegex = BuildTagRegex("p");
-        private static readonly Regex StrongTagRegex = BuildTagRegex("strong");
-        private static readonly Regex MultipleSpaceRegex = new RegexBuilder()
-            .Whitespace(OneOrMore)
-            .BuildRegex();
-
-        private static readonly Regex HtmlTagRegex = new RegexBuilder()
-            .Text("<")
-            .AnyCharacter(OneOrMore.ButAsFewAsPossible)
-            .Text(">")
-            .BuildRegex();
-
-        private static readonly Regex RawQuestionAnswerRegex = new RegexBuilder()
-            .StartOfString()
-            // Question/answer number
-            .StartGroup()
-            .Digit(OneOrMore)
-            .EndGroup()
-            .Whitespace(OneOrMore)
-            // Question/answer text
-            .StartGroup()
-            .AnyCharacter(OneOrMore.ButAsFewAsPossible)
-            .EndGroup()
-            .Text(".", ZeroOrOne)
-            .PossibleWhitespace()
-            .EndOfString()
-            .BuildRegex();
-            
         public IEnumerable<Question> FindQuestions(string html)
         {
-            var htmlLines = html
-                .Split("\n")
-                .Select(line => line.Trim())
-                .Where(line => line.StartsWith("<p>", StringComparison.OrdinalIgnoreCase))
-                .Where(line => MinQuestionNumbers.All(number => line.Contains(number.ToString())))
-                .Select(SanitiseHtml)
-                .ToList();
+            var sections = _sectionExtractor.ExtractSections(html);
 
-            if (htmlLines.Count != 2)
-            {
-                throw new ParsingException($"Found {htmlLines.Count} matching lines in source HTML (expected 2)");
-            }
+            var questionsSection = _htmlStripper.StripHtml(sections.QuestionsSectionHtml);
+            var answersSection = _htmlStripper.StripHtml(sections.AnswersSectionHtml);
 
-            // Parse questions
-            var questions = ParseRawQuestions(htmlLines[0]).ToList();
-            
-            // Parse answers
-            ParseRawAnswers(htmlLines[1], questions);
+            var questionsSectionSplit = _sectionSplitter.SplitSection(questionsSection);
+            var answersSectionSplit = _sectionSplitter.SplitSection(answersSection);
+
+            var questions = _questionAssembler.AssembleQuestions(
+                questionsSectionSplit,
+                answersSectionSplit);
             
             return questions;
         }
-
-        private static IEnumerable<Question> ParseRawQuestions(string rawQuestionsHtml)
-        {
-            var questionSections = WhatLinksRegex
-                .Split(rawQuestionsHtml)
-                .ToList();
-
-            if (questionSections.Count != 2)
-            {
-                throw new ParsingException($"Found {questionSections.Count} question section(s) (expected 2)");
-            }
-            
-            var normalQuestions = ParseRawQuestionSection(questionSections.First(), QuestionType.Normal);
-            var whatLinksQuestions = ParseRawQuestionSection(questionSections.Last(), QuestionType.WhatLinks);
-
-            var questions = new List<Question>();
-            questions.AddRange(normalQuestions);
-            questions.AddRange(whatLinksQuestions);
-
-            return questions;
-        }
-
-        private static IEnumerable<Question> ParseRawQuestionSection(string rawQuestionSectionHtml, QuestionType type)
-        {
-            var questions = new List<Question>();
-            
-            var rawQuestions = BrTagRegex
-                .Split(rawQuestionSectionHtml)
-                .Where(q => !string.IsNullOrWhiteSpace(q));
-
-            foreach (var rawQuestion in rawQuestions)
-            {
-                var regexMatch = RawQuestionAnswerRegex.Match(rawQuestion);
-                if (!regexMatch.Success)
-                {
-                    throw new ParsingException($"Could not parse {rawQuestion}");
-                }
-                
-                questions.Add(new Question
-                {
-                    Number = int.Parse(regexMatch.Groups[1].Value),
-                    Type = type,
-                    QuestionHtml = regexMatch.Groups[2].Value,
-                    QuestionText = MakeTextSafe(regexMatch.Groups[2].Value)
-                });
-            }
-
-            return questions;
-        }
-
-        private static void ParseRawAnswers(string rawAnswersHtml, IEnumerable<Question> questions)
-        {
-            var rawAnswers = BrTagRegex
-                .Split(rawAnswersHtml)
-                .Where(q => !string.IsNullOrWhiteSpace(q));
-
-            foreach (var rawAnswer in rawAnswers)
-            {
-                var regexMatch = RawQuestionAnswerRegex.Match(rawAnswer);
-                if (!regexMatch.Success)
-                {
-                    throw new ParsingException($"Could not parse {rawAnswer}");
-                }
-
-                var questionNumber = int.Parse(regexMatch.Groups[1].Value);
-                var answerHtml = regexMatch.Groups[2].Value;
-                var question = questions.Single(q => q.Number == questionNumber);
-                question.AnswerHtml = answerHtml;
-                question.AnswerText = MakeTextSafe(answerHtml);
-            }
-        }
-        
-        private static string SanitiseHtml(string html)
-        {
-            var sanitisedHtml = html
-                .Remove(PTagRegex)
-                .Remove(StrongTagRegex)
-                .Remove(ATagRegex)
-                .Replace("&nbsp;", " ");
-
-            sanitisedHtml = MultipleSpaceRegex.Replace(sanitisedHtml, " ");
-
-            return sanitisedHtml;
-        }
-
-        private static string MakeTextSafe(string source)
-        {
-            if (string.IsNullOrEmpty(source))
-            {
-                return source;
-            }
-            
-            var safeSource = HttpUtility.HtmlDecode(source);
-            return safeSource.Remove(HtmlTagRegex);
-        }
-
-        private static Regex BuildTagRegex(string tagName) => new RegexBuilder()
-            .Text("<")
-            .Text("/", ZeroOrOne)
-            .Text(tagName)
-            .AnyCharacterExcept(">", ZeroOrMore)
-            .Text(">")
-            .BuildRegex(RegexOptions.IgnoreCase);
     }
 }
