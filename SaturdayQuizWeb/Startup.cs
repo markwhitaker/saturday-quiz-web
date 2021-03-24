799e9dcb332684872209f0f25fa2d8728c002455
Mark Whitaker <mark@mainwave.co.uk>
2021-03-24 16:54:31 +0000

    Fix enum serialisation

--- SaturdayQuizWeb/Startup.cs
+++ SaturdayQuizWeb/Startup.cs
@@ -15,7 +15,7 @@ namespace SaturdayQuizWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddSwaggerGen(c =>
             {
