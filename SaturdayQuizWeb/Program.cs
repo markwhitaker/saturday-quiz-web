84193b02c80504d64c17cfaaa3ed10948902d34a
Mark Whitaker <mark@mainwave.co.uk>
2021-04-09 15:27:13 +0100

    Remove unnecessary call to builder.AddEnvironmentVariables()

--- SaturdayQuizWeb/Program.cs
+++ SaturdayQuizWeb/Program.cs
@@ -14,11 +14,8 @@ namespace SaturdayQuizWeb
         private static IHostBuilder CreateHostBuilder(string[] args) => Host
             // This step adds UserSecrets as a configuration source
             // (see https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets#register-the-user-secrets-configuration-source)
+            // It also registers environment variables as a configuration source (see method comment)
             .CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((_, builder) =>
-            {
-                builder.AddEnvironmentVariables();
-            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
