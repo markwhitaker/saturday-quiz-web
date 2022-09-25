a09df39be39a89cf8efc7cb2d4020431a12ee98b
Mark Whitaker <markw@bitrot.net>
2022-09-25 09:02:32 +0100

    Enable caching in web app

--- SaturdayQuizWeb/wwwroot/script/QuizRepository.js
+++ SaturdayQuizWeb/wwwroot/script/QuizRepository.js
@@ -1,14 +1,31 @@
 import Quiz from "./Quiz.js";
+import QuizMetadata from "./QuizMetadata.js";
 
 export default class QuizRepository {
-    #apiEndpoint = Object.freeze("api/quiz");
+    #quizEndpoint = Object.freeze("api/quiz?id=");
+    #quizMetadataEndpoint = Object.freeze("api/quiz-metadata?count=1");
 
     async loadLatestQuiz() {
-        const response = await fetch(this.#apiEndpoint);
+        const metadata = await this.#loadQuizMetadata();
+        const apiEndpoint = this.#quizEndpoint + metadata.id;
+        const response = await fetch(apiEndpoint);
         if (!response.ok) {
-            throw new Error("Failed to fetch " + this.#apiEndpoint + ". " + response.status + ": " + response.statusText);
+            this.#error(apiEndpoint, response)
         }
         const quizJson = await response.json();
         return new Quiz(quizJson);
+    }
+
+    async #loadQuizMetadata() {
+        const response = await fetch(this.#quizMetadataEndpoint);
+        if (!response.ok) {
+            this.#error(this.#quizMetadataEndpoint, response)
+        }
+        const quizMetadataJson = await response.json();
+        return new QuizMetadata(quizMetadataJson[0]);
+    }
+
+    #error(endpoint, response) {
+        throw new Error("Failed to fetch " + endpoint + ". " + response.status + ": " + response.statusText);
     }
 }
