3846a0175e4ad7ddd8bac59ff33eaa94fd3ca80b
Mark Whitaker <markw@bitrot.net>
2022-11-04 22:21:37 +0000

    Add spacing between properties (#100)

--- SaturdayQuizWeb/Model/Parsing/Sections.cs
+++ SaturdayQuizWeb/Model/Parsing/Sections.cs
@@ -3,5 +3,6 @@
 public sealed record Sections
 {
     public string QuestionsSectionHtml { get; init; } = string.Empty;
+
     public string AnswersSectionHtml { get; init; } = string.Empty;
 }
--- SaturdayQuizWeb/Model/Quiz.cs
+++ SaturdayQuizWeb/Model/Quiz.cs
@@ -3,7 +3,10 @@
 public sealed record Quiz
 {
     public string Id { get; init; } = string.Empty;
+
     public DateTime Date { get; init; }
+
     public string Title { get; init; } = string.Empty;
+
     public IEnumerable<QuestionModel> Questions { get; init; } = new List<QuestionModel>();
 }
