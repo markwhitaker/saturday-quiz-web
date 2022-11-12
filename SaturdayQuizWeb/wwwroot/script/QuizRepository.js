77a2c33edd56e5cf2724f8501c68c0db27e7387b
Mark Whitaker <markw@bitrot.net>
2022-11-12 21:27:34 +0000

    Simplify QuizRepository.js

--- SaturdayQuizWeb/wwwroot/script/QuizRepository.js
+++ SaturdayQuizWeb/wwwroot/script/QuizRepository.js
@@ -2,30 +2,14 @@ import Quiz from "./Quiz.js";
 import QuizMetadata from "./QuizMetadata.js";
 
 export default class QuizRepository {
-    #quizEndpoint = Object.freeze("api/quiz?id=");
-    #quizMetadataEndpoint = Object.freeze("api/quiz-metadata?count=1");
+    #quizEndpoint = Object.freeze("api/quiz");
 
     async loadLatestQuiz() {
-        const metadata = await this.#loadQuizMetadata();
-        const apiEndpoint = this.#quizEndpoint + metadata.id;
-        const response = await fetch(apiEndpoint);
+        const response = await fetch(this.#quizEndpoint);
         if (!response.ok) {
-            this.#error(apiEndpoint, response)
+            throw new Error(`Failed to fetch ${this.#quizEndpoint}. ${response.status}: ${response.statusText}`);
         }
         const quizJson = await response.json();
         return new Quiz(quizJson);
-    }
-
-    async #loadQuizMetadata() {
-        const response = await fetch(this.#quizMetadataEndpoint);
-        if (!response.ok) {
-            this.#error(this.#quizMetadataEndpoint, response)
-        }
-        const quizMetadataJson = await response.json();
-        return new QuizMetadata(quizMetadataJson[0]);
-    }
-
-    #error(endpoint, response) {
-        throw new Error("Failed to fetch " + endpoint + ". " + response.status + ": " + response.statusText);
     }
 }
