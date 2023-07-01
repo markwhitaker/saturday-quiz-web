550fc5eaa8a73346f9606046eb6e036dd1974e9c
Mark Whitaker <markw@bitrot.net>
2023-07-01 12:24:34 +0100

    Fix TS errors

--- SaturdayQuizWeb/Scripts/QuizRepository.ts
+++ SaturdayQuizWeb/Scripts/QuizRepository.ts
@@ -3,7 +3,7 @@ import Quiz from "./Quiz.js";
 export default class QuizRepository {
     private static readonly QUIZ_ENDPOINT = "api/quiz";
 
-    async loadLatestQuiz() : Promise<Quiz> {
+    async loadLatestQuiz() {
         const response = await fetch(QuizRepository.QUIZ_ENDPOINT);
         if (!response.ok) {
             throw new Error(`Failed to fetch ${(QuizRepository.QUIZ_ENDPOINT)}. ${response.status}: ${response.statusText}`);
