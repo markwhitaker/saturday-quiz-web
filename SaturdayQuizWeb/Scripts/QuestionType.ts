f351432477237ca2d39fc0969ec2b06ea1c3c6e3
Mark Whitaker <markw@bitrot.net>
2023-07-01 11:33:32 +0100

    Convert QuestionType to enum

--- SaturdayQuizWeb/Scripts/QuestionType.ts
+++ SaturdayQuizWeb/Scripts/QuestionType.ts
@@ -1,6 +1,6 @@
-const QuestionType = {
-    NORMAL: "NORMAL",
-    WHAT_LINKS: "WHAT_LINKS"
-} as const;
+enum QuestionType {
+    NORMAL = "NORMAL",
+    WHAT_LINKS = "WHAT_LINKS"
+}
 
 export default QuestionType;
--- SaturdayQuizWeb/wwwroot/js/QuestionType.js
+++ SaturdayQuizWeb/wwwroot/js/QuestionType.js
@@ -1,6 +1,7 @@
-const QuestionType = {
-    NORMAL: "NORMAL",
-    WHAT_LINKS: "WHAT_LINKS"
-};
+var QuestionType;
+(function (QuestionType) {
+    QuestionType["NORMAL"] = "NORMAL";
+    QuestionType["WHAT_LINKS"] = "WHAT_LINKS";
+})(QuestionType || (QuestionType = {}));
 export default QuestionType;
 //# sourceMappingURL=QuestionType.js.map
