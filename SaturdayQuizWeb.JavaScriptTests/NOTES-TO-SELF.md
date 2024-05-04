bab9440fa70ee82e061997de8c476e08dfecd4a7
Mark Whitaker <markw@bitrot.net>
2024-05-04 13:36:52 +0100

    Trying to get Jest + ES6 modules to work

---
+++ SaturdayQuizWeb.JavaScriptTests/NOTES-TO-SELF.md
@@ -0,0 +1,7 @@
+# Links
+
+- https://chat.openai.com/share/e/10c2cfd2-4d3a-4348-9107-09dd71c884f6
+
+- https://jestjs.io/docs/ecmascript-modules
+
+- https://stackoverflow.com/a/69059786/440921
---
+++ SaturdayQuizWeb.JavaScriptTests/Presenter.test.js
@@ -0,0 +1,6 @@
+import Presenter from "../SaturdayQuizWeb/wwwroot/script/Presenter.js";
+
+test('2 + 2 should be 4', () => {
+    const presenter = new Presenter();
+    expect(presenter).toBeDefined();
+});
