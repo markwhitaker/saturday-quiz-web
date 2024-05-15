0dd51f29377702cece5d3f01b15a8a06a446481d
Mark Whitaker <markw@bitrot.net>
2024-05-15 06:56:41 +0100

    Get tests working and tidy up

--- SaturdayQuizWeb.JavaScriptTests/tests/Presenter.test.js
+++ SaturdayQuizWeb.JavaScriptTests/tests/Presenter.test.js
@@ -1,4 +1,4 @@
-import Presenter from "../SaturdayQuizWeb/wwwroot/script/Presenter.js";
+import Presenter from "../../SaturdayQuizWeb/wwwroot/script/Presenter.js";
 
 test('2 + 2 should be 4', () => {
     const presenter = new Presenter();
