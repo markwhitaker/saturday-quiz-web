491eb693686e508740d5dff09efe8c018bf24b3a
Mark Whitaker <markw@bitrot.net>
2024-05-15 06:50:13 +0100

    Get Mocha tests working

---
+++ SaturdayQuizWeb.JavascriptTests/test/QuizTests.js
@@ -0,0 +1,6 @@
+import assert from 'assert';
+import QuestionScore from '../../SaturdayQuizWeb/wwwroot/script/QuestionScore.js';
+
+it('QuestionScore.NONE should be 0', () => {
+    assert.equal(QuestionScore.NONE, 0);
+});
---
+++ SaturdayQuizWeb.JavascriptTests/test/test.js
@@ -0,0 +1,5 @@
+import assert from 'assert';
+
+it('should add to numbers from an es module', () => {
+    assert.equal(3 + 5, 8);
+});
