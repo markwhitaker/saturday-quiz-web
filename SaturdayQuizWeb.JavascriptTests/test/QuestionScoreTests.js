35b36751c61f7b26c19d757c542ba23f9bdb89fa
Mark Whitaker <markw@bitrot.net>
2024-05-15 07:08:45 +0100

    Add QuestionScore tests

--- SaturdayQuizWeb.JavascriptTests/test/QuestionScoreTests.js
+++ SaturdayQuizWeb.JavascriptTests/test/QuestionScoreTests.js
@@ -4,3 +4,11 @@ import QuestionScore from '../../SaturdayQuizWeb/wwwroot/script/QuestionScore.js
 it('QuestionScore.NONE should be 0', () => {
     assert.equal(QuestionScore.NONE, 0);
 });
+
+it('QuestionScore.HALF should be 0.5', () => {
+    assert.equal(QuestionScore.HALF, 0.5);
+});
+
+it('QuestionScore.FULL should be 1', () => {
+    assert.equal(QuestionScore.FULL, 1);
+});
--- SaturdayQuizWeb.JavascriptTests/test/test.js
+++
@@ -1,5 +0,0 @@
-import assert from 'assert';
-
-it('should add to numbers from an es module', () => {
-    assert.equal(3 + 5, 8);
-});
