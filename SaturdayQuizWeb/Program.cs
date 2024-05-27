ed40f484b728c225f13e6a59e1f67cb4a04d9c33
Mark Whitaker <markw@bitrot.net>
2024-05-27 15:18:32 +0100

    Simplify Program.cs

--- SaturdayQuizWeb/Program.cs
+++ SaturdayQuizWeb/Program.cs
@@ -1,17 +1,10 @@
 ﻿using SaturdayQuizWeb;
 
-// See https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/program-structure/top-level-statements
-// for top-level statement syntax
-
-CreateHostBuilder(args).Build().Run();
-return;
-
-IHostBuilder CreateHostBuilder(string[] args) => Host
+Host
     // This step adds UserSecrets as a configuration source
     // (see https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets#register-the-user-secrets-configuration-source)
     // It also registers environment variables as a configuration source (see method comment)
     .CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.UseStartup<Startup>();
-    });
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .Build()
+    .Run();
