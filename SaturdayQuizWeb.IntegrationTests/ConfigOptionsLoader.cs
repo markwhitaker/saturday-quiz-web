e99acbd024fdb95df658106c6b547beb73529351
Mark Whitaker <markw@bitrot.net>
2024-06-01 09:51:22 +0100

    Implement lazy loading in ConfigOptionsLoader

--- SaturdayQuizWeb.IntegrationTests/ConfigOptionsLoader.cs
+++ SaturdayQuizWeb.IntegrationTests/ConfigOptionsLoader.cs
@@ -6,24 +6,25 @@ namespace SaturdayQuizWeb.IntegrationTests;
 
 public static class ConfigOptionsLoader
 {
-    public static IOptions<GuardianConfig> ConfigOptions
+    private static IOptions<GuardianConfig>? _configOptions;
+
+    public static IOptions<GuardianConfig> ConfigOptions => _configOptions ??= BuildConfigOptions();
+
+    private static IOptions<GuardianConfig> BuildConfigOptions()
     {
-        get
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", false, false)
-                .AddUserSecrets<QuizMetadataServiceTests>()
-                .AddEnvironmentVariables()
-                .Build();
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, false)
+            .AddUserSecrets<QuizMetadataServiceTests>()
+            .AddEnvironmentVariables()
+            .Build();
 
-            var services = new ServiceCollection()
-                .Configure<GuardianConfig>(configuration.GetSection(Constants.ConfigSectionGuardian))
-                .BuildServiceProvider();
+        var services = new ServiceCollection()
+            .Configure<GuardianConfig>(configuration.GetSection(Constants.ConfigSectionGuardian))
+            .BuildServiceProvider();
 
-            var configOptions = services.GetService<IOptions<GuardianConfig>>() ?? throw new Exception(
-                $"Failed to get IOptions<{nameof(GuardianConfig)}> from service provider");
+        var configOptions = services.GetService<IOptions<GuardianConfig>>() ?? throw new Exception(
+            $"Failed to get IOptions<{nameof(GuardianConfig)}> from service provider");
 
-            return configOptions;
-        }
+        return configOptions;
     }
 }
