02d21f40cc2f1b05782b517eab1977c7c6f218c8
Mark Whitaker <markw@bitrot.net>
2024-06-05 09:20:53 +0100

    Add QuizRepository.tests.js

--- SaturdayQuizWeb.JavaScriptTests/test/mocks/LocalStoreMockBuilder.js
+++ SaturdayQuizWeb.JavaScriptTests/test/mocks/LocalStoreMockBuilder.js
@@ -1,8 +1,27 @@
 export default class LocalStoreMockBuilder {
     #mock = {
+        getQuizDate: () => {},
+        setQuizDate: () => {},
+
+        getQuiz: () => {},
+        setQuiz: () => {},
+        clearQuiz: () => {},
+
         getScores: () => {},
         setScores: () => {},
 
+        get quizDate() {
+            return this.getQuizDate();
+        },
+        set quizDate(value) {
+            this.setQuizDate(value);
+        },
+        get quiz() {
+            return this.getQuiz();
+        },
+        set quiz(value) {
+            this.setQuiz(value);
+        },
         get scores() {
             return this.getScores();
         },
@@ -10,6 +29,31 @@ export default class LocalStoreMockBuilder {
             this.setScores(value);
         }
     };
+
+    getQuizDate(fn) {
+        this.#mock.getQuizDate = fn;
+        return this;
+    }
+
+    setQuizDate(fn) {
+        this.#mock.setQuizDate = fn;
+        return this;
+    }
+
+    getQuiz(fn) {
+        this.#mock.getQuiz = fn;
+        return this;
+    }
+
+    setQuiz(fn) {
+        this.#mock.setQuiz = fn;
+        return this;
+    }
+
+    clearQuiz(fn) {
+        this.#mock.clearQuiz = fn;
+        return this;
+    }
 
     getScores(fn) {
         this.#mock.getScores = fn;
---
+++ SaturdayQuizWeb/wwwroot/script/FetchWrapper.js
@@ -0,0 +1,5 @@
+export default class FetchWrapper {
+    async fetch(url) {
+        return await fetch(url);
+    }
+}
