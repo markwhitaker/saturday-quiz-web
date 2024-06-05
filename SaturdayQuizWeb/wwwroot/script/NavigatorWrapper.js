27a466450f14fe179a4e763a1fe807f51dca16b1
Mark Whitaker <markw@bitrot.net>
2024-06-05 13:48:16 +0100

    Fix NavigatorWrapper

--- SaturdayQuizWeb/wwwroot/script/NavigatorWrapper.js
+++ SaturdayQuizWeb/wwwroot/script/NavigatorWrapper.js
@@ -1,6 +1,6 @@
 export default class NavigatorWrapper {
     get isShareSupported() {
-        return navigator.share !== undefined;
+        return navigator?.share !== undefined;
     }
 
     async share(data) {
