4ba6d60071a24e59c76885f66bb1a6e36c1154a6
Mark Whitaker <markw@bitrot.net>
2024-07-03 08:08:47 +0100

    Clear Accept headers in GuardianWebsiteHttpClient

--- SaturdayQuizWeb/Clients/HttpClients/GuardianWebsiteHttpClient.cs
+++ SaturdayQuizWeb/Clients/HttpClients/GuardianWebsiteHttpClient.cs
@@ -8,6 +8,7 @@ public class GuardianWebsiteHttpClient(HttpClient httpClient, IOptions<GuardianC
     public async Task<string> GetStringAsync(string endpoint)
     {
         httpClient.BaseAddress ??= new Uri(configOptions.Value.WebsiteBaseUrl);
+        httpClient.DefaultRequestHeaders.Accept.Clear();
         return await httpClient.GetStringAsync(endpoint);
     }
 }
