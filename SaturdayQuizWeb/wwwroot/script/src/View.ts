717fc904a7f9323bbdcdec4102f2459d8c80b4b1
Mark Whitaker <mark@bitrot.net>
2025-07-11 09:58:42 +0100

    Fix implicit types in View.ts

--- SaturdayQuizWeb/wwwroot/script/src/View.ts
+++ SaturdayQuizWeb/wwwroot/script/src/View.ts
@@ -42,15 +42,15 @@ export default class View {
     };
 
     enableNavigation = (): void => {
-        elements.navLeft().on("click", e => {
+        elements.navLeft().on("click", (e: JQuery.ClickEvent<HTMLElement, undefined, HTMLElement, HTMLElement>) => {
             this.#presenter.onPrevious();
             e.preventDefault();
         });
-        elements.navRight().on("click", e => {
+        elements.navRight().on("click", (e: JQuery.ClickEvent<HTMLElement, undefined, HTMLElement, HTMLElement>) => {
             this.#presenter.onNext();
             e.preventDefault();
         });
-        elements.document().on("keyup", e => {
+        elements.document().on("keyup", (e: JQuery.KeyUpEvent<Document, undefined, Document, Document>) => {
             switch (e.code) {
                 case 'ArrowLeft':
                     this.#presenter.onPrevious();
@@ -67,17 +67,17 @@ export default class View {
             e.preventDefault();
         });
 
-        elements.scoreTick().on("click", e => {
+        elements.scoreTick().on("click", (e: JQuery.ClickEvent<HTMLElement, undefined, HTMLElement, HTMLElement>) => {
             this.#presenter.toggleScore();
             e.preventDefault();
         });
 
-        elements.scoreShare().on("click", async e => {
+        elements.scoreShare().on("click", async (e: JQuery.ClickEvent<HTMLElement, undefined, HTMLElement, HTMLElement>) => {
             await this.#presenter.shareScore();
             e.preventDefault();
         });
 
-        elements.skipToAnswers().on("click", e => {
+        elements.skipToAnswers().on("click", (e: JQuery.ClickEvent<HTMLElement, undefined, HTMLElement, HTMLElement>) => {
             this.#presenter.toggleSkipToAnswers();
             e.preventDefault();
         });
